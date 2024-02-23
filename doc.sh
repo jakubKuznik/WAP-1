c9ef0a012dbc3f06d7aa3e44f218a94be54451b1
jakubKuznik <kuznja16@gmail.com>
2024-02-23 16:38:50 +0100

    install scipts

--- doc.sh
+++ doc.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-npm install --save-dev 
-npm run jsdoc 
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    npm run jsdoc 
+else
+    if [ "$1" = "install" ]; then
+        npm install --save-dev 
+    else
+        echo "Unknown argument (know only install)"
+    fi
+fi
+
--- test.sh
+++ test.sh
@@ -3,11 +3,10 @@
 
 if [ $# -eq 0 ]; then
     npm test 
-fi
-
-if [ "$1" = "install" ]; then
-    npm install --save-dev
 else
-    echo "Unknown argument (know only install)"
+    if [ "$1" = "install" ]; then
+        npm install --save-dev
+    else
+        echo "Unknown argument (know only install)"
+    fi
 fi
-
